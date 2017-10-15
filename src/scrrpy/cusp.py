2d9fcffa5aa2dda28b139a8c7e2dbe2ca1b8042d
Ben Bar-Or <benbaror@gmail.com>
2017-10-15 15:33:22 -0400

    Added initial code

---
+++ src/scrrpy/cusp.py
@@ -0,0 +1,170 @@
+"""Properties of the stellar cusp"""
+import numpy as np
+from numpy import sqrt
+from numpy import pi
+from astropy import constants
+from scipy.special import eval_legendre
+
+G = constants.G
+M0 = constants.M_sun
+C = constants.c
+RG0 = (G*M0/C**2).to('pc').value
+TG0 = (G*M0/C**3).to('year').value
+
+
+
+class Cusp(object):
+    """
+    """
+
+    def __init__(self, gamma=1.75, mbh=4e6, mstar=1, rh=2):
+        """
+        """
+        self.gamma = gamma
+        self.mbh = mbh
+        self.mstar = mstar
+        self.rh = rh
+
+    @property
+    def rg(self, ):
+        """
+        Gravitational rads in pc
+        """
+        return RG0*self.mbh
+    @property
+    def tg(self, ):
+        """
+        Gravitational time
+        """
+        return TG0*self.mbh
+
+    @property
+    def Q(self):
+        """
+        """
+        return self.mbh/self.mstar
+
+    @property
+    def Nh(self):
+        """
+        """
+        return self.Q
+
+    def Nstars(self, a):
+        """
+        The number of stars within a[pc]
+        """
+        return self.Nh*(a/self.rh)**(3-self.gamma)
+
+    def Mstars(self, a):
+        """
+        The number of stars within a[pc]
+        """
+        return self.mbh*(a/self.rh)**(3-self.gamma)
+
+    def dMda(self, a):
+        """
+        dM/da at a[pc]
+        """
+        return (3-self.gamma)*self.mbh*(a/self.rh)**(3-self.gamma)/a
+
+    def period(self, a):
+        """
+        The orbital period in years at a[pc]
+        """
+        return 2*pi/self.nu_r(a)
+
+    def nu_r(self, a):
+        """
+        The orbital frequency in rad/year at a[pc]
+        """
+        return (self.rg/a)**(1.5)/self.tg
+
+    def nu_mass(self, a, j):
+        """
+        The frequency of the mass precession
+        """
+        return self._nu_mass0(a)*self._g(j)
+
+    def _nu_mass0(self, a):
+        """
+        The frequency of the mass precession divided by g(j)
+        """
+        return -self.nu_r(a)*self.Mstars(a)/self.mbh
+
+    def _g(self, j):
+        """
+        g(j) = -j^(6-2\gamma)/pi/sqrt(1-j^2)*\int_0^\pi cos(s)/(1+sqrt(1+j^2)^(3-\gamma)
+        For \gamma = 2:
+            g(j) = j/(1+j)
+        For \gamma = 1:
+            g(j) = j
+        For any \gamma
+        g(1) = (3-\gamma)/2
+        """
+        P1 = eval_legendre(1 - self.gamma, 1/j)
+        P2 = eval_legendre(2 - self.gamma, 1/j)
+        return - j**(4 - self.gamma)/(1-j**2)*(P1 - P2/j)
+
+    def _gp(self, j):
+        """
+        dg(j)/dj
+        """
+        P1 = eval_legendre(1 - self.gamma, 1/j)
+        P2 = eval_legendre(2 - self.gamma, 1/j)
+        P3 = eval_legendre(3 - self.gamma, 1/j)
+        return -(j**(2 - self.gamma)/(1-j**2)**2 *
+                 (j*(6-2*self.gamma + (self.gamma - 2)*j**2)*P1 +
+                  (2*self.gamma-6-j**2)*P2 -
+                  (self.gamma-3)*j*P3))
+
+    def nu_gr(self, a, j):
+        """
+        The frequency of the GR precession
+        """
+        return self.nu_r(a)*3*(self.rg/a)/j**2
+
+    def nu_p(self, a, j):
+        """
+        The frequency of the precession
+        """
+        return self.nu_gr(a, j) + self.nu_mass(a, j)
+
+    def nu_p1(self, a):
+        """
+        The frequency of the precession at j=1
+        """
+        return self.nu_gr(a, 1.0) + self._nu_mass0(a)*(3-self.gamma)/2
+
+    def d_nu_p(self, a, j):
+        """
+        The derivative of \nu_p with respect to j defined to be positive
+        """
+        return (2*self.nu_gr(a, j)/j - self._nu_mass0(a)*self._gp(j))
+
+    def nu_mass_inv(self, j, omega):
+        """
+        Return a such that nu_mass(a,j) = omega
+        """
+        nu_mass_rh = abs(self.nu_mass(self.rh, j))
+        return (omega/nu_mass_rh)**(1/(3/2-self.gamma))*self.rh
+
+    def inverse_cumulative_a(self, r):
+        return r**(1/(3-self.gamma))*self.rh
+
+
+    @property
+    def a_gr1(self):
+        """
+        The below which nup is only positive,
+        that is nup(a,j=1) = 0
+        """
+        return ((6/(3-self.gamma)*self.Q/self.Nh*self.rg/self.rh)
+                **(1/(4-self.gamma)) *
+                self.rh)
+
+    def jlc(self, a):
+        """
+        Relativistic loss cone
+        """
+        return 4*np.sqrt(self.rg/a)
---
+++ src/scrrpy/drr.py
@@ -0,0 +1,205 @@
+from cusp import Cusp
+import numpy as np
+import vegas
+import progressbar
+from scipy import special
+
+
+class DRR(Cusp):
+    """
+    """
+
+    def __init__(self, at, l=1, gamma=1.75, mbh=4e6, mstar=1, rh=2):
+        """
+
+        Arguments:
+        - `l`: multipole order
+        - `gamma`: slope of the density profile
+        - `mbh`: Black hole mass [solar mass]
+        - `mstar`: mass of individual stars  [solar mass]
+        - `rh`: radius of influence [pc]
+        """
+        super().__init__(gamma=gamma, mbh=mbh, mstar=mstar, rh=rh)
+        self.at = at
+        self.j = np.logspace(np.log10(self.jlc(at)), 0, 101)[:-1]
+        self.omega = abs(self.nu_p(self.at, self.j))
+        self.l = l
+
+    def res_int(self, ratio):
+        try:
+            return self._res_int[ratio]
+        except AttributeError:
+            self._res_int = {}
+        except KeyError:
+            pass
+        self._res_int[ratio] = Res_interp(self, self.omega*ratio)
+        return self._res_int[ratio]
+
+
+    def _integrand(self, a, j, ap, jp, l, n, n_p, true_anomaly):
+        return (2*jp/abs(self.d_nu_p(ap, jp))/n_p *
+                A2_integrand(a, j, ap, jp, l, n, n_p, true_anomaly))
+
+    def drr(self, l, n, n_p, neval=1e3):
+        key = '{},{},{}'.format(l, n, n_p)
+        try:
+            return self._drr_dict[key]
+        except AttributeError:
+            self._drr_dict = {}
+        except KeyError:
+            pass
+        self._drr_dict[key] = np.zeros([self.j.size, 2])
+
+        bar = progressbar.ProgressBar()
+        for ji, omegai, i in zip(self.j, self.omega, bar(range(self.j.size))):
+            self._drr_dict[key][i, :] = self._drr(self.at, ji, omegai,
+                                               l, n, n_p, neval=neval)
+
+        return self._drr_dict[key]
+
+    def _drr(self, a, j, omega, l, n, n_p, neval=1e3):
+        integ = vegas.Integrator(5 * [[0, 1]])
+        ratio = n/n_p
+
+        @vegas.batchintegrand
+        def C(x):
+            true_anomaly = x[:, :-1].T*np.pi
+            af = self.inverse_cumulative_a(x[:, -1])
+            jf1 = self.res_int(ratio).get_jf1(omega*ratio, af)
+            jf2 = self.res_int(ratio).get_jf2(omega*ratio, af)
+            x = np.zeros_like(af)
+            ix1 = jf1 > 0
+            ix2 = jf2 > 0
+            x[ix1] = self._integrand(a, j, af[ix1], jf1[ix1], l, n, n_p,
+                                     true_anomaly[:, ix1])
+            x[ix2] += self._integrand(a, j, af[ix2], jf2[ix2], l, n, n_p,
+                                      true_anomaly[:, ix2])
+            return x
+        return np.array(integrate(C, integ, neval))*self._A2_norm_factor(l, n, n_p)*n**2
+
+    def _A2_norm_factor(self, l, n, n_p):
+        """
+        Normalization factor for |alnnp|^2
+        ! To be implemented
+        """
+        key = '{},{},{}'.format(l, n, n_p)
+        try:
+            return self._A2_norm[key]
+        except AttributeError:
+            self._A2_norm = {}
+        except KeyError:
+            pass
+        self._A2_norm[key] = _A2_norm_factor(l, n, n_p)
+        return self._A2_norm[key]
+
+
+
+def integrate(func, integ, neval):
+    result = integ(func, nitn=10, neval=neval)
+    result = integ(func, nitn=10, neval=neval)
+    try:
+        return np.array([[r.val, np.sqrt(r.var)] for r in result]).T
+    except TypeError:
+        return result.val, np.sqrt(result.var)
+
+
+def A2_integrand(a, j, ap, jp, l, n, n_p, true_anomaly):
+    """
+    returns the |alnnp|^2 integrand to use the the MC integration
+    """
+    c = np.prod(np.cos(true_anomaly.T*np.array([n, n, n_p, n_p])), 1)
+    ecc, eccp = np.sqrt(1-j**2), np.sqrt(1-jp**2)
+    r1, r2 = (a*(1-ecc**2)/(1-ecc*np.cos(true_anomaly[:2])))
+    rp1, rp2 = (ap*(1-eccp**2)/(1-eccp*np.cos(true_anomaly[2:])))
+    return (1/j/jp*c/j**2/jp**2/a**2/ap**4 *
+            (np.minimum(r1, rp1)*np.minimum(r2, rp2))**(2*l+1) /
+            (r1*r2*rp1*rp2)**(l-1))
+
+
+
+
+def _A2_norm_factor(l, n, n_p):
+    """
+    Normalization factor for |alnnp|^2
+    ! To be implemented
+    """
+    return (abs(special.sph_harm(n, l, 0, np.pi/2))**2 *
+            abs(special.sph_harm(n_p, l, 0, np.pi/2))**2 *
+            (4*np.pi/(2*l + 1)))**2
+
+
+class Res_interp(object):
+    """
+    Interpolation function for the resonant condition
+    """
+
+    def __init__(self, cusp, omega):
+        """
+        """
+        self._cusp = cusp
+        self.omega = omega
+        self._af = np.logspace(np.log10(self._cusp.rg),
+                               np.log10(self._cusp.rh),
+                               1000)
+        # self._jf = np.logspace(np.log10(self._cusp.jlc(self._cusp.rh)),
+        #                                 0, 1001)[:-1]
+
+        def get_j(nup):
+            jf = self._jf[nup > 0]
+            nup = nup[nup > 0]
+            s = np.argsort(nup)
+            j = np.interp(self.omega, nup[s], jf[s], left=0, right=0)
+            # j[self.omega < nup.min()] = 0
+            # j[self.omega > nup.max()] = 0
+            return j
+
+        # The minimal a at which omega changes sign.
+        a_gr1 = self._cusp.a_gr1
+        # The minimal at which omega intersects nu_p
+        self._af = np.logspace(np.log10(self._cusp.rg),
+                               np.log10(self._cusp.rh),
+                               1000)
+
+        a_min = self._af[(self._af < a_gr1) *
+                         (omega.max() < self._cusp.nu_p1(self._af))].max()
+        self._af = np.logspace(np.log10(a_min),
+                               np.log10(self._cusp.rh),
+                               1000)
+
+        self._j1 = np.zeros([self._af.size, self.omega.size])
+        self._j2 = np.zeros([self._af.size, self.omega.size])
+
+        for i, a in enumerate(self._af[self._af < a_gr1]):
+            self._jf = np.logspace(np.log10(self._cusp.jlc(a)),
+                                   0, 1001)[:-1]
+            nup = self._cusp.nu_p(a, self._jf)
+            self._j1[i, :] = get_j(nup)
+
+        last = i + 1
+        for i, a in enumerate(self._af[self._af > a_gr1]):
+            self._jf = np.logspace(np.log10(self._cusp.jlc(a)),
+                                   0, 1001)[:-1]
+            nup = self._cusp.nu_p(a, self._jf)
+            self._j1[i+last, :] = get_j(nup)
+            if any(nup < 0):
+                self._j2[i+last, :] = get_j(-nup)
+
+    def get_jf1(self, omega, af):
+        i = np.argmin(abs(self.omega-omega))
+        if abs(self.omega[i]-omega) > 1e-8:
+            raise ValueError
+        j = self._j1[:, i]
+        if sum(j > 0):
+            return np.interp(af, self._af[j > 0], j[j > 0], left=0, right=0)
+        else:
+            return af*0.0
+
+    def get_jf2(self, omega, af):
+        i = np.argmin(abs(self.omega-omega))
+        if abs(self.omega[i]-omega) > 1e-8:
+            raise ValueError
+        j = self._j2[:, i]
+        if sum(j > 0):
+            return np.interp(af, self._af[j > 0], j[j > 0], left=0, right=0)
+        else:
+            return af*0.0

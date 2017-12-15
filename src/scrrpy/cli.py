92548d3e5e45cc168aa57493715c96f675122852
Ben Bar-Or <benbaror@gmail.com>
2017-12-15 18:11:26 -0500

    Created a command line interface

--- src/scrrpy/cli.py
+++ src/scrrpy/cli.py
@@ -14,10 +14,28 @@ Why does this file exist, and why not put this in __main__?
 
   Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
-import click
 
+import click
+import matplotlib.pyplot as plt
+from .drr import DRR
 
 @click.command()
-@click.argument('names', nargs=-1)
-def main(names):
-    click.echo(repr(names))
+@click.argument('name')
+@click.argument('sma', type=click.FLOAT)
+@click.argument('l_max', type=click.INT)
+@click.option('--gamma', default=1.75)
+@click.option('--mstar', default=1.0)
+@click.option('--mbh', default=4e6)
+@click.option('--rh', default=2.0)
+@click.option('--threads', default=1)
+@click.option('--neval', default=1e4)
+@click.option('--plot', is_flag=True)
+def main(name, sma, l_max, gamma, mstar, mbh, rh, threads, neval, plot):
+    drr = DRR(sma, gamma=gamma, mbh=mbh, mstar= mstar, rh=rh)
+    Drr, Drr_err =  drr.drr(l_max, threads=threads, tol=0.0, neval=neval)
+    drr.save(name + '.hdf5')
+    if plot:
+        plt.errorbar(drr.j, Drr, Drr_err, fmt='o')
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.savefig(name + '.eps')

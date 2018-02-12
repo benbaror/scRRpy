f41b186e599381e0a0046962535463159f2c6229
Ben Bar-Or <benbaror@gmail.com>
2018-02-12 17:43:51 -0500

    Added docs and cleaned up code

--- src/scrrpy/drr.py
+++ src/scrrpy/drr.py
@@ -2,12 +2,16 @@
 A module for calculating Resonant Relaxation diffusion coefficients
 """
 
+from __future__ import absolute_import, division, print_function
+
 import multiprocessing as mp
 from ast import literal_eval as make_tuple
+from builtins import range, super, zip
 from functools import lru_cache
 
 import h5py
 import numpy as np
+# noinspection PyPackageRequirements
 import progressbar
 import vegas
 from numba import jit
@@ -20,39 +24,44 @@ from .cusp import Cusp
 
 class DRR(Cusp):
     """
-    Resonant relaxation diffusion coefficient
+    Resonant relaxation diffusion coefficient (DRR)
+    Assuming a power law stellar cusp around a massive black hole (MBH).
+    The cusp is assumed to have an isotropic distribution function
+    :math:`f(E) \propto |E|^p` corresponding ro a stellar density
+    :math:`n(r) \propto r^{-\gamma}` where :math:`\gamma = 3/2 + p`
+
+    Parameters
+    ----------
+    sma : float
+       The semi-mahor axis along which DRR will be computed
+
+    gamma : float, int, optional
+        The slope of the density profile.
+        default: 7/4 (Bahcall wolf cusp)
+    mbh_mass : float, int, optional
+        Mass of the MBH [solar mass].
+        default: 4.3 10^6 (Milky Way MBH)
+    star_mass : float, int, optional
+        Mass of individual stars [solar mass].
+        default: 1.0
+    rh : float, int, optional
+        Radius of influence [pc].
+        Define as the radius in which the velocity
+        dispersion of the stellar cusp :math:`\sigma` is equal to the
+        Keplerian velocity due to the MBH
+        :math:`\sigma(r_h)^2 = G M_\bullet / r_h`.
+        default: 2.0
     """
 
-    def __init__(self, sma, **kwargs):
-        """
+    def __init__(self, sma, gamma=1.75, mbh_mass=4e6, star_mass=1.0, j_grid_size=128, rh=2.0):
 
-        Arguments:
-        - `sma`: semi-major axis [pc]
-        - `l_sphr`: multipole order
-        - `gamma`: slope of the density profile
-        - `mbh`: Black hole mass [solar mass]
-        - `mstar`: mass of individual stars  [solar mass]
-        - `rh`: radius of influence [pc]
-        """
-
-        # Default arguments
-        kwargs = {**dict(gamma=1.75,
-                         mbh=4e6,
-                         mstar=1,
-                         Njs=201,
-                         gr_factor=1,
-                         rh=2),
-                  **kwargs}
-
-        super().__init__(gamma=kwargs['gamma'],
-                         mbh_mass=kwargs['mbh'],
-                         star_mass=kwargs['mstar'],
-                         rh=kwargs['rh'])
-
+        super().__init__(gamma=gamma,
+                         mbh_mass=mbh_mass,
+                         star_mass=star_mass,
+                         rh=rh)
         self.sma = sma
-        self.gr_factor = kwargs['gr_factor']
-        self.j = np.logspace(np.log10(self.jlc(self.sma)), 0,
-                             kwargs['Njs'])[:-1]
+        self.gr_factor = 1.0
+        self.j = np.logspace(np.log10(self.jlc(self.sma)), 0, j_grid_size + 1)[:-1]
         self.omega = abs(self.nu_p(self.sma, self.j))
 
     @lru_cache()
@@ -61,13 +70,15 @@ class DRR(Cusp):
 
     def _integrand(self, j, sma_p, j_p, lnnp, true_anomaly):
         d_nu_p = abs(self.d_nu_p(sma_p, j_p))
-        A2_int = A2_integrand(self.sma, j, sma_p, j_p, lnnp, true_anomaly)
-        return 2 * j_p * A2_int / d_nu_p / lnnp[-1]
+        a2_int = a2_integrand(self.sma, j, sma_p, j_p, lnnp, true_anomaly)
+        return 2 * j_p * a2_int / d_nu_p / lnnp[-1]
 
-    def drr(self, l_max, neval=1e3, threads=1, tol=0.0, progress_bar=True):
+    def __call__(self, l_max, neval=1e3, threads=1, tol=0.0, progress_bar=True):
         """
         Returns the RR diffusion coefficient over Jc^2 in 1/yr.
         """
+
+        # Get all non-vanishing resonances up to l=l_max
         lnnp = [(l, n, n_p)
                 for l in range(1, l_max + 1)
                 for n in range(1, l + 1)
@@ -84,8 +95,8 @@ class DRR(Cusp):
             self._drr_lnnp(*lnnp[i], neval=neval, threads=threads, tol=tol)
 
         drr = np.vstack([self._drr_lnnp(*lnnp, neval=neval, threads=threads,
-                                  tol=tol)[0]
-                   for lnnp in  lnnp])
+                                        tol=tol)[0]
+                         for lnnp in lnnp])
 
         # Remove non-physical values
         drr[drr < 0] = 0
@@ -124,32 +135,26 @@ class DRR(Cusp):
 
             def parallel_drr(pos, seed, j, omega):
                 np.random.seed(seed)
-                results = [self._drr(j, omega, (l, n, n_p), neval=neval,
-                                     tol=tol)
-                           for j, omega in zip(j, omega)]
-                drr = [result[0] for result in results]
-                drr_err = [result[-1] for result in results]
-                queue.put((pos, (drr, drr_err)))
+                _results = [self._drr(j, omega, (l, n, n_p), neval=neval,
+                                      tol=tol)
+                            for j, omega in zip(j, omega)]
+                _drr = [result[0] for result in _results]
+                _drr_err = [result[-1] for result in _results]
+                queue.put((pos, (_drr, _drr_err)))
 
             js = [self.j[i::threads] for i in range(threads)]
             omegas = [self.omega[i::threads] for i in range(threads)]
-            r = np.arange(self.j.size)
-            resort = np.argsort(np.concatenate([r[i::threads]
-                                                for i in range(threads)]))
+
             seeds = np.random.randint(100000, size=threads)
-            processes = [mp.Process(target=parallel_drr, args=(i, *x))
-                         for i, x in enumerate(zip(seeds, js, omegas))]
+            processes = [mp.Process(target=parallel_drr, args=(i, seed, j, omega))
+                         for i, (seed, j, omega) in enumerate(zip(seeds, js, omegas))]
             for process in processes:
                 process.start()
             for process in processes:
                 process.join()
 
-            results = [queue.get() for p in processes]
-            results.sort()
-            drr = np.concatenate([result[-1][0] for result in results])[resort]
-
-            drr_err = np.concatenate([result[-1][1]
-                                      for result in results])[resort]
+            _, results = zip(*sorted(map(queue.get, processes)))
+            drr, drr_err = np.hstack(results)
 
         else:
             results = [self._drr(j, omega, (l, n, n_p), neval=neval, tol=tol)
@@ -172,49 +177,13 @@ class DRR(Cusp):
                                                  tol))][-1] ** 2)
         return drr, drr_err
 
-    def tau2(self, l_max, neval=1e3, include_zero=True):
-        n_p_min = 0 if include_zero else 1
-        tau2 = sum([self._tau2_lnnp(l, n, n_p, neval=neval)[0]
-                    for l in range(1, l_max + 1)
-                    for n in range(1, l + 1)
-                    for n_p in range(n_p_min, l + 1)
-                    if not mod(l + n, 2) + mod(l + n_p, 2)])
-
-        tau2_err = sqrt(sum([self._tau2_lnnp(l, n, n, neval=neval)[-1] ** 2
-                             for l in range(1, l_max + 1)
-                             for n in range(1, l + 1)
-                             for n_p in range(n_p_min, l + 1)
-                             if not mod(l + n, 2) + mod(l + n_p, 2)]))
-        return tau2, tau2_err
-
-    @lru_cache()
-    def _tau2_lnnp(self, l, n, n_p, neval=1e3):
-        """
-        Calculates the l,n,n_p term of the diffusion coefficient
-        """
-        tau2, tau2_err = np.zeros([2, self.j.size])
-
-        widgets = ['{}, {}, {}'.format(l, n, n_p), ' ',
-                   progressbar.Percentage(),
-                   ' (', progressbar.SimpleProgress(), ')', ' ',
-                   progressbar.Bar(), ' ',
-                   progressbar.Timer(), ' ',
-                   progressbar.AdaptiveETA()]
-
-        pbar = progressbar.ProgressBar(widgets=widgets)
-
-        for j_i, omegai, i in zip(self.j, self.omega,
-                                  pbar(range(self.j.size))):
-            tau2[i], tau2_err[i] = self._tau2(j_i, [l, n, n_p], neval=neval)
-        return tau2, tau2_err
-
     def _drr(self, j, omega, lnnp, neval=1e3, tol=0.0):
         ratio = lnnp[1] / lnnp[-1]
         get_jf1 = self._res_intrp(ratio).get_jf1(omega * ratio)
         get_jf2 = self._res_intrp(ratio).get_jf2(omega * ratio)
 
         @vegas.batchintegrand
-        def Clnnp1(x):
+        def c_lnnp1(x):
             true_anomaly = x[:, :-1].T * np.pi
             sma_f = self.inverse_cumulative_a(x[:, -1])
             jf1 = get_jf1(sma_f)
@@ -226,7 +195,7 @@ class DRR(Cusp):
             return res
 
         @vegas.batchintegrand
-        def Clnnp2(x):
+        def c_lnnp2(x):
             true_anomaly = x[:, :-1].T * np.pi
             sma_f = self.inverse_cumulative_a(x[:, -1])
             jf2 = get_jf2(sma_f)
@@ -242,51 +211,16 @@ class DRR(Cusp):
         if get_jf1 is None:
             int1, err1 = 0.0, 0.0
         else:
-            int1, err1 = np.array(integrate(Clnnp1, integ, neval, tol))
+            int1, err1 = np.array(integrate(c_lnnp1, integ, neval, tol))
 
         if get_jf2 is None:
             int2, err2 = 0.0, 0.0
         else:
-            int2, err2 = np.array(integrate(Clnnp2, integ, neval, tol))
+            int2, err2 = np.array(integrate(c_lnnp2, integ, neval, tol))
 
         return 4 * np.pi * (np.array([[int1, int2], [err1, err2]]) *
-                            _A2_norm_factor(*lnnp) * lnnp[1] ** 2 *
+                            _a2_norm_factor(*lnnp) * lnnp[1] ** 2 *
                             self.nu_r(self.sma) ** 2 / self.mass_ratio ** 2 * self.total_number_of_stars)
-
-    def _tau2(self, j, lnnp, neval=1e3):
-        if np.mod(lnnp[0] + lnnp[1], 2) or np.mod(lnnp[0] + lnnp[-1], 2):
-            return 0, 0
-
-        tau2, tau2_err = np.zeros([2, self.j.size])
-
-        integ = vegas.Integrator(6 * [[0, 1]])
-        sma = self.sma
-        gamma = self.gamma
-        rh = self.rh
-        l, n, n_p = lnnp
-        j2 = j ** 2
-        ecc = sqrt(1 - j ** 2)
-
-        @vegas.batchintegrand
-        def Clnnp(x):
-            true_anomaly = x[:, :-2].T * np.pi
-            sma_p = (x[:, -2]) ** (1 / (3 - gamma)) * rh
-            j2_p = x[:, -1]
-            cnnp = np.cos(true_anomaly.T * np.array([n, n, n_p, n_p])).T
-            cnnp = cnnp[0] * cnnp[1] * cnnp[2] * cnnp[3]
-            eccp = sqrt(1 - j2_p)
-            r12 = sma * j2 / (1 - ecc * np.cos(true_anomaly[:2]))
-            rp12 = sma_p * j2_p / (1 - eccp * np.cos(true_anomaly[2:]))
-            r_1, r_2 = r12[0], r12[-1]
-            rp1, rp2 = rp12[0], rp12[-1]
-            return (cnnp / j2_p / sma_p ** 4 *
-                    (np.minimum(r_1, rp1) * np.minimum(r_2, rp2)) ** (2 * l + 1) /
-                    (r_1 * r_2 * rp1 * rp2) ** (l - 1))
-
-        # Symmetry factor for using n>0 and n_p >= 0
-        symmetry_factor = 2 if n_p == 0 else 4
-        return symmetry_factor * (np.array(integrate(Clnnp, integ, neval)) *
-                                  _A2_norm_factor(*lnnp) * lnnp[1] ** 2) / j2 / sma ** 2
 
     @property
     def l_max(self):
@@ -326,7 +260,7 @@ class DRR(Cusp):
                 except TypeError:
                     pass
 
-    def read(self, file_name):
+    def _read(self, file_name):
         """
         Read the cached data from an hdf5 file
         """
@@ -349,13 +283,12 @@ class DRR(Cusp):
         example:
         drr = DRR.from_file(file_name)
         """
-        return cls(1.0).read(file_name)
+        return cls(1.0)._read(file_name)
 
 
 def integrate(func, integ, neval=1e4, tol=0.0):
-    n = neval
     integ.set(rtol=tol)
-    result = integ(func, nitn=10, neval=neval)
+    integ(func, nitn=10, neval=neval)
     result = integ(func, nitn=10, neval=neval)
     try:
         res, err = np.array([[r.val, np.sqrt(r.var)] for r in result]).T
@@ -365,7 +298,7 @@ def integrate(func, integ, neval=1e4, tol=0.0):
 
 
 @jit(nopython=True)
-def A2_integrand(sma, j, sma_p, j_p, lnnp, true_anomaly):
+def a2_integrand(sma, j, sma_p, j_p, lnnp, true_anomaly):
     """
     returns the |alnnp|^2 integrand to use in the MC integration
     """
@@ -385,7 +318,7 @@ def A2_integrand(sma, j, sma_p, j_p, lnnp, true_anomaly):
 
 
 @lru_cache()
-def _A2_norm_factor(l, n, n_p):
+def _a2_norm_factor(l, n, n_p):
     """
     Normalization factor for |alnnp|^2
     """
@@ -413,11 +346,11 @@ class ResInterp(object):
         # self._jf = np.logspace(np.log10(self._cusp.jlc(self._cusp.rh)),
         #                                 0, 1001)[:-1]
 
-        def get_j(nup):
-            jf = self._jf[nup > 0]
-            nup = nup[nup > 0]
-            s = np.argsort(nup)
-            j = np.interp(self.omega, nup[s], jf[s], left=0, right=0)
+        def get_j(_nup):
+            jf = self._jf[_nup > 0]
+            _nup = _nup[_nup > 0]
+            s = np.argsort(_nup)
+            j = np.interp(self.omega, _nup[s], jf[s], left=0, right=0)
 
             # j[self.omega < nup.min()] = 0
             # j[self.omega > nup.max()] = 0
@@ -446,7 +379,7 @@ class ResInterp(object):
             nup = self._cusp.nu_p(a, self._jf)
             self._j1[i, :] = get_j(nup)
             last += 1
-        #last += 1
+        # last += 1
 
         for i, a in enumerate(self._af[self._af > a_gr1]):
             self._jf = np.logspace(np.log10(self._cusp.jlc(a)),

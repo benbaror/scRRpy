fc1a6e1b2a8ff79e4aa7ded62bab67c3e891a9e0
Ben Bar-Or <benbaror@gmail.com>
2018-02-13 21:22:06 -0500

    Removed python 2.7 support and updated configurations

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
         re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
     ),
-    author='Ben Bar-Or',
+    author='Ben Bar-Or and Jean-Baptiste Fouvry',
     author_email='benbaror@gmail.com',
     url='https://github.com/benbaror/scrrpy',
     packages=find_packages('src'),
@@ -41,31 +41,25 @@ setup(
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha'
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        # uncomment if you test on these interpreters:
-        # 'Programming Language :: Python :: Implementation :: IronPython',
-        # 'Programming Language :: Python :: Implementation :: Jython',
-        # 'Programming Language :: Python :: Implementation :: Stackless',
-        'Topic :: Utilities',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Scientific/Engineering :: Physics',
     ],
     keywords=[
-        # eg: 'keyword1', 'keyword2', 'keyword3',
+         'astronomy', 'astrophysics', 'science', 'diffusion', 'massive black holes', 'galactic center',
+         'general relativity', 'statistical mechanics', 'stellar dynamics'
     ],
     install_requires=[
-        'click', 'h5py', 'numpy', 'progressbar2', 'vegas', 'numba', 'numpy', 'scipy', 'astropy', 'matplotlib',
+        'click', 'numpy', 'scipy', 'progressbar2', 'vegas', 'numba', 'astropy', 'matplotlib', 'h5py'
     ],
     extras_require={
         # eg:

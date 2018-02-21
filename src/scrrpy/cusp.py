f0c976ae56b89c7bb24fc1985ae78e318954ab83
Ben Bar-Or <benbaror@gmail.com>
2018-02-21 16:28:26 -0500

    Better formatting of the equations

--- src/scrrpy/cusp.py
+++ src/scrrpy/cusp.py
@@ -1,5 +1,7 @@
 """Properties of the stellar cusp"""
 
+from functools import partial
+
 import numpy as np
 # noinspection PyUnresolvedReferences
 # noinspection PyUnresolvedReferences
@@ -7,19 +9,17 @@ import numpy as np
 from astropy.constants import G
 from astropy.constants import M_sun
 from astropy.constants import c
-# from functools import partial
 from numba import jit
+from numba.errors import TypingError
 from numpy import pi
 from scipy.special import eval_legendre
-
-# from numba.errors import TypingError
 
 M0 = M_sun
 # Define physical constants
 # Gravitational radius of a solar mass black hole
-RG0 = (G*M_sun/c**2).to('pc').value
+RG0 = (G * M_sun / c ** 2).to('pc').value
 # Light crossing time of solar mass black hole
-TG0 = (G*M_sun/c**3).to('year').value
+TG0 = (G * M_sun / c ** 3).to('year').value
 
 
 # noinspection PyCompatibility
@@ -68,14 +68,14 @@ class Cusp(object):
         """
         Gravitational radius of the MBH [pc]
         """
-        return RG0*self.mbh_mass
+        return RG0 * self.mbh_mass
 
     @property
     def tg(self):
         """
         Light crossing time of the MBH [sec]
         """
-        return TG0*self.mbh_mass
+        return TG0 * self.mbh_mass
 
     def jlc(self, a):
         """
@@ -152,7 +152,7 @@ class Cusp(object):
         a: float, array
             Semi-major axis [pc].
         """
-        return 2*pi/self.nu_r(a)
+        return 2 * pi / self.nu_r(a)
 
     def nu_r(self, a):
         """
@@ -163,7 +163,7 @@ class Cusp(object):
         a: float, array
             Semi-major axis [pc].
         """
-        return (self.rg/a)**1.5/self.tg
+        return (self.rg / a) ** 1.5 / self.tg
 
     def nu_mass(self, a, j):
         """
@@ -176,7 +176,7 @@ class Cusp(object):
         j: float, array
             Normalized angular momentum :math:`j = J/J_c = \sqrt{1-e^2}`.
         """
-        return self._nu_mass0(a)*self._g(j)
+        return self._nu_mass0(a) * self._g(j)
 
     def nu_gr(self, a, j):
         """
@@ -190,7 +190,7 @@ class Cusp(object):
         j: float, array
             Normalized angular momentum :math:`j = J/J_c = \sqrt{1-e^2}`.
         """
-        return self.gr_factor*self.nu_r(a)*3*(self.rg/a)/j**2
+        return self.gr_factor * self.nu_r(a) * 3 * (self.rg / a) / j ** 2
 
     def nu_p(self, a, j):
         """
@@ -210,15 +210,16 @@ class Cusp(object):
         """
         Precession frequency at j=1
         """
-        return self.nu_gr(a, 1.0) + self._nu_mass0(a)*(3-self.gamma)/2
+        return self.nu_gr(a, 1.0) + self._nu_mass0(a) * (3 - self.gamma) / 2
 
     def d_nu_p(self, a, j):
         """
         The derivative of nu_p with respect to j, defined to be positive
         """
-        d_nu_gr = 6*self.gr_factor*(self.rg/a)/(j*j*j)
-        d_nu_mass = -self.stellar_mass(a) / self.mbh_mass * self._gp(j)
-        return (d_nu_gr - d_nu_mass)*self.nu_r(a)
+        d_nu_gr = 6 * self.gr_factor * (self.rg / a) / (j * j * j)
+        d_nu_mass = self._gp(j)
+        d_nu_mass = - d_nu_mass * self.stellar_mass(a) / self.mbh_mass
+        return (d_nu_gr - d_nu_mass) * self.nu_r(a)
 
     def inverse_cumulative_a(self, x):
         """
@@ -238,7 +239,7 @@ class Cusp(object):
         >>> print("{:0.10}, {:0.10}, {:0.10}".format(sma.min(), sma.mean(), sma.max()))
         0.03430996478, 1.147418232, 1.987320281
         """
-        return x**(1/(3-self.gamma))*self.rh
+        return x ** (1 / (3 - self.gamma)) * self.rh
 
     def _nu_mass0(self, a):
         """
@@ -259,7 +260,7 @@ class Cusp(object):
         """
         p1 = self._eval_legendre_inv(1 - self.gamma, j)
         p2 = self._eval_legendre_inv(2 - self.gamma, j)
-        return - j**(4 - self.gamma)/(1-j**2)*(p1 - p2/j)
+        return - j ** (4 - self.gamma) / (1 - j ** 2) * (p1 - p2 / j)
 
     def _gp(self, j):
         """
@@ -277,41 +278,37 @@ class Cusp(object):
         that is nup(a,j=1) = 0
         """
         return ((self.gr_factor *
-                 6 / (3-self.gamma) * self.mass_ratio /
+                 6 / (3 - self.gamma) * self.mass_ratio /
                  self.total_number_of_stars * self.rg / self.rh) **
-                (1/(4-self.gamma)) *
+                (1 / (4 - self.gamma)) *
                 self.rh)
 
-    @staticmethod
-    def _eval_legendre_inv(n, j):
-        return eval_legendre(n, 1/j)
-
-    # def _eval_legendre_inv_int(self, n, j):
-    #     try:
-    #         return self._eval_legendre_inv[n](j)
-    #     except (AttributeError, KeyError, TypingError) as err:
-    #         if type(err) is TypingError:
-    #             return self._eval_legendre_inv[n](np.atleast_1d(j))[0]
-    #         if type(err) is AttributeError:
-    #             self._eval_legendre_inv = {}
-    #         j_samp = np.logspace(np.log10(self.jlc(self.rh)), 0, 1000)
-    #         pn = eval_legendre(n, 1/j_samp)
-    #         x0 = np.log(j_samp[0])
-    #         dx_inv = 1/(np.log(j_samp[1]) - x0)
-    #         self._eval_legendre_inv[n] = partial(interp_loglog, x0=x0,
-    #                                              dx_inv=dx_inv,
-    #                                              logf=np.log(pn))
-    #         return self._eval_legendre_inv[n](j)
+    def _eval_legendre_inv(self, n, j):
+        try:
+            return self._eval_legendre_inv_cache[n](j)
+        except (AttributeError, KeyError, TypingError) as err:
+            if type(err) is TypingError:
+                return self._eval_legendre_inv_cache[n](np.atleast_1d(j))[0]
+            if type(err) is AttributeError:
+                self._eval_legendre_inv_cache = {}
+            j_samp = np.logspace(np.log10(self.jlc(self.rh)), 0, 1000)
+            pn = eval_legendre(n, 1 / j_samp)
+            x0 = np.log(j_samp[0])
+            dx_inv = 1 / (np.log(j_samp[1]) - x0)
+            self._eval_legendre_inv_cache[n] = partial(interp_loglog, x0=x0,
+                                                       dx_inv=dx_inv,
+                                                       logf=np.log(pn))
+        return self._eval_legendre_inv(n, j)
 
 
 @jit(nopython=True)
 def interp_semilogx(x_int, x0, dx_inv, f):
     f_int = np.empty_like(x_int)
     for i in range(x_int.size):
-        x_dx = (np.log(x_int[i])-x0)*dx_inv
+        x_dx = (np.log(x_int[i]) - x0) * dx_inv
         ind = int(x_dx)
         w = x_dx - ind
-        f_int[i] = f[ind]*(1-w) + f[ind+1]*w
+        f_int[i] = f[ind] * (1 - w) + f[ind + 1] * w
     return f_int
 
 
@@ -319,14 +316,14 @@ def interp_semilogx(x_int, x0, dx_inv, f):
 def interp_loglog(x_int, x0, dx_inv, logf):
     f_int = np.empty_like(x_int)
     for i, x in enumerate(x_int):
-        x_dx = (np.log(x)-x0)*dx_inv
+        x_dx = (np.log(x) - x0) * dx_inv
         ind = int(x_dx)
         w = x_dx - ind
-        f_int[i] = logf[ind]*(1-w) + logf[ind+1]*w
+        f_int[i] = logf[ind] * (1 - w) + logf[ind + 1] * w
     return np.exp(f_int)
 
 
 @jit(nopython=True)
 def gp(j, p1, p2, n2):
-    return (j**n2 / (1 - j**2)**2 *
-            ((j**2 + 1)*p2 - j*(2 + n2*(1 - j**2))*p1))
+    return (j ** n2 / (1 - j ** 2) ** 2 *
+            ((j ** 2 + 1) * p2 - j * (2 + n2 * (1 - j ** 2)) * p1))
